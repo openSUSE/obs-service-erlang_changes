282a8b75cc6f28b905e394d3660f1d0e9630470e
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-12-01 16:00:09 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='obs-service-erlang_changes',
+	version='0.1.0',
+	description='',
+	url='http://github.com/matwey/obs-service-erlang_changes',
+	author='Matwey V. Kornilov',
+	author_email='matwey.kornilov@gmail.com',
+	license='GPL-2.0',
+	packages=['erlang_changes'],
+	test_suite = 'tests',
+	zip_safe=False)
