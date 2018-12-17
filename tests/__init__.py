bed944891231762e4081e1d0f244725d45a350b4
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-12-17 15:29:15 +0300

    Add TestChanges to tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 from tests.notes_xml import TestNotesXML
 from tests.otp_versions_table import TestOTPVersionsTable
 from tests.service_data import TestServiceData
+from tests.changes import TestChanges
 
-__all__ = ["TestNotesXML", "TestOTPVersionsTable", "TestServiceData"]
+__all__ = ["TestNotesXML", "TestOTPVersionsTable", "TestServiceData", "TestChanges"]
