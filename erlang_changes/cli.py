99a3463d5b03e8ce72b02fb39e15b6763ec5d6af
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2020-02-09 15:57:23 +0300

    Drop _servicedata

--- erlang_changes/cli.py
+++ erlang_changes/cli.py
@@ -4,7 +4,6 @@ import glob
 import shutil
 import erlang_changes
 from erlang_changes.changes import Changes
-from erlang_changes.service_data import ServiceData, ServiceDataEmpty
 
 try:
 	FileNotFoundError
@@ -30,17 +29,7 @@ def execute_from_commandline(argv=None):
 	otp_src = erlang_changes.OTPSrc.from_file(otp_src_filename)
 
 	otp_version = otp_src.otp_version
-
-	servicedata_filename = "_servicedata"
-	tmp_servicedata_filename = os.path.join(args.outdir, "_servicedata")
-
-	try:
-		servicedata = ServiceData(servicedata_filename)
-	except (ServiceDataEmpty, FileNotFoundError):
-		servicedata = ServiceData.init_servicedata(servicedata_filename, otp_version)
-		servicedata.write(servicedata_filename)
-
-	prev_otp_version = servicedata.otp_version
+	prev_otp_version = 
 
 	if prev_otp_version == otp_version:
 		return
@@ -53,11 +42,6 @@ def execute_from_commandline(argv=None):
 		tmp_changes = os.path.join(args.outdir, changes)
 		shutil.copyfile(os.path.join(path, changes), tmp_changes)
 		new_changes.write(tmp_changes)
-
-	servicedata.set_otp_version(otp_version)
-	servicedata.write(tmp_servicedata_filename)
-
-	shutil.move(tmp_servicedata_filename, servicedata_filename)
 
 	for path, changes in Changes.find_changes():
 		tmp_changes = os.path.join(args.outdir, changes)
