721f2b28ab862e02d632500551b1b5d41dd0d9cf
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2024-09-23 20:06:16 +0300

    Initial implementation for pyproject.toml

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from tests.notes_xml import TestNotesXML
-from tests.otp_versions_table import TestOTPVersionsTable
-from tests.changes import TestChanges
-
-__all__ = ["TestNotesXML", "TestOTPVersionsTable", "TestChanges"]
